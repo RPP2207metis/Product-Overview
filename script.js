b52aaf5ed119a852834536e0b2c73ec020efdbc1
Aaron Miller <AaronMiller1855@gmail.com>
2023-02-14 20:05:20 -0700

    push up loader io authentication route

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ import { check, sleep, Rate } from "k6";
   let min = 900000;
   const productId = Math.floor(Math.random() * (max - min) + min);
 
-// Define the base URL of the API Routes
+/*==================
+  Testing Routes for Localhost
+===================*/
+
+/*
 const allProductsroute = `http://localhost:3000/products/`;
 const productRequest = `http://localhost:3000/products/${productId}`;
 const styleRequest = `http://localhost:3000/products/${productId}/styles`;
 const relatedRoute = `http://localhost:3000/products/${productId}/related`;
+*/
+
+/*==================
+  Testing Routes for public IP
+====================*/
+
+const allProductsroute = `http://54.185.5.152/products/`;
+const productRequest = `http://54.185.5.152/products/${productId}`;
+const styleRequest = `http://54.185.5.152/products/${productId}/styles`;
+const relatedRoute = `http://54.185.5.152/products/${productId}/related`;
 
 export const options = {
   discardResponseBodies: true,
@@ -21,13 +35,13 @@ export const options = {
       rate: 1,
       timeUnit: '1s',
       preAllocatedVUs: 2,
-      maxVUs: 50000,
+      maxVUs: 10,
     },
   },
 };
 
 export default function() {
-  let response = http.get(`${productRequest}`, options);
+  let response = http.get(`${styleRequest}`, options);
   check(response, {
     "status is 200": (r) => r.status === 200
   });
