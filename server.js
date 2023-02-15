7f4a9163d4b64c41f1d8a2655bfa7dbca2a39fe5
Aaron Miller <AaronMiller1855@gmail.com>
2023-02-14 20:38:23 -0700

    loaderio test

--- server.js
+++ server.js
@@ -37,8 +37,9 @@ app.get('/products/:product_id/styles', productStyles)
 app.get('/products/:product_id/related', productRelated)
 
 /* Flood.io Authentication */
-app.get('/loaderio-0739c1300dab3514075ec644cc486eaa/' , (req, res) => {
-  res.send(process.env.LOADERIO)
+app.get('/loaderio-0739c1300dab3514075ec644cc486eaa' , (req, res) => {
+  // res.send(process.env.LOADERIO)
+  res.sendStatus(200)
 })
 
 /*==================
